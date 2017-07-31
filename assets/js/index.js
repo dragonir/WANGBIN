e1ef20048e8dcebd1042cc8f169241f7fdfc4737
dragonir <dragonir@sina.cn>
2017-07-31 11:17:13 +0800

    visit count

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,3 +42,41 @@ function timeFormat(i){
 	}
 	return i;
 }
+
+
+// 访问次数
+function getCookieVal(offset){
+var endstr=document.cookie.indexOf(";",offset);
+if(endstr==-1)
+endstr=document.cookie.length;
+return unescape(document.cookie.substring(offset,endstr));
+}
+ 
+function GetCookie(name){
+var arg=name+"=";
+var alen=arg.length;
+var clen=document.cookie.length;var i=0;
+while(i<clen){
+var j=i+alen;
+if(document.cookie.substring(i,j)==arg)
+return getCookieVal(j);
+i=document.cookie.indexOf(" ",i)+1;if(i==0)
+break;
+}
+return null;
+}
+ 
+function SetCookie(name,value){
+var argv=SetCookie.arguments;
+var argc=SetCookie.arguments.length;
+var expires=(2<argc)?argv[2]:null;
+var path=(3<argc)?argv[3]:null;
+var domain=(4<argc)?argv[4]:null;
+var secure=(5<argc)?argv[5]:false;
+document.cookie=name+"="+escape(value)+((expires==null)?"":("; expires="+expires.toGMTString()))+((path==null)?"":("; path="+path))+((domain==null)?"":("; domain="+domain))+((secure==true)?"; secure":"");
+}
+ 
+function ResetCounts(name){
+visits=0;SetCookie("visits",visits,expdate,"/",null,false);
+location.reload();
+}
